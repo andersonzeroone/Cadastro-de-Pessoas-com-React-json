ae9c9b814c5c560395efb2ef4c1bf3ad5427e416
anderson <anderson.pablo02@gmail.com>
2019-06-16 11:05:26 -0400

    Subindo

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+import { BrowserRouter } from   'react-router-dom'
+
+
+import Logo from '../componets/template/Logo'
+import Nav from '../componets/template/Nav'
+import Routes from   './Routes'
+import Footer from '../componets/template/Footer'
+
+
+export default props => 
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
